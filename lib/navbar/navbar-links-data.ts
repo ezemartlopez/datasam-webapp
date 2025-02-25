ad5354e4f48f2f7618ddaa976aff7ea423113eb0
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 19:47:30 -0300

    links del nav de la pagina web

---
+++ lib/navbar/navbar-links-data.ts
@@ -0,0 +1,12 @@
+export interface NavbarLink{
+  name: string;
+  href: string;
+}
+
+export const navbar_links: NavbarLink[] = [
+  {name: "Inicio", href: "/"},
+  {name: "Comunidad", href: "/comunidad"},
+  {name: "Carrera", href: "/carrera"},
+]
+
+
