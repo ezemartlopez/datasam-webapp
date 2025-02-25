55801c7ccf4789c866904dfb742ee83e9ccf3df2
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 19:46:22 -0300

    modificando la pagina base de la app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import "./globals.css";
-import Navbar from "@/components/shared/navbar";
 import { RootStoreProvider } from "@/components/providers/root-store-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+import Navbar from "@/components/shared/navbar";
+import type { Metadata } from "next";
+import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Rubik } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+export const rubikFont = Rubik({
+  variable: "--font-rubik", // Variable para la fuente en CSS
+  subsets: ["latin"], 
 });
 
 export const metadata: Metadata = {
   title: "DATA SAM",
-  description:
-    "DATA SAM: Donde el conocimiento se comparte y la comunidad se fortalece 🚀",
+  description: "DATA SAM una comunidad de estudiantes de licenciatura en ciencia de datos de la UNSAM, donde el conocimiento se comparte y la comunidad se fortalece",
+  icons: {
+    icon: "/images/logo_v_claro_final.svg",  // Ruta del ícono en la carpeta 'public'
+  },
 };
 
 export default function RootLayout({
@@ -30,9 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <RootStoreProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${rubikFont.variable} antialiased`}>
           <Navbar />
           <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 md:gap-8">
             {children}
