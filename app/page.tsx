692e78dbc3a71fff573c2c68b93d9d203b7242da
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 22:59:04 -0300

    pagina principal modificada

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
-import { About } from "@/components/sections/about";
-import { Footer } from "@/components/sections/footer";
-import { ParticlesHome } from "@/components/sections/particles-home";
-import { UsefulLinksHome } from "@/components/sections/useful-links-home";
+//import { About } from "@/components/sections/about";
+//import { Footer } from "@/components/sections/footer";
+import AboutCommunity from "@/components/sections/home/about-community";
+import MainHome from "@/components/sections/home/main";
+import SocialLinks from "@/components/sections/home/social-links";
+//import { ParticlesHome } from "@/components/sections/particles-home";
+//import { UsefulLinksHome } from "@/components/sections/useful-links-home";
 
 export default function Home() {
   return (
-    <div>
-      <ParticlesHome />
+    <div className="w-full h-full">
+      <MainHome/>
+      <AboutCommunity/>
+      <SocialLinks/>
+      {/*<ParticlesHome />
 
       <div className="relative z-10 p-4 bg-white md:p-10">
         <About />
         <UsefulLinksHome />
         <Footer />
       </div>
+      */}
     </div>
   );
 }
