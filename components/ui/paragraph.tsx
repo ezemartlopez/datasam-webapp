84d26f4cfad5cf6c088e59264f1978a2b4f3271a
ezemartlopez <zenitram.e14@gmail.com>
2025-02-26 16:04:24 -0300

    modificacion del parrafo

--- components/ui/paragraph.tsx
+++ components/ui/paragraph.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 // Component: Paragraph.tsx
 export default function Paragraph({ text, bold }: { text: string; bold?: boolean }) {
   return (
-    <p className={clsx("font-rubik text-lg sm:text-xl font-light text-wrap text-left antialiased text-gray-800", { "font-normal": bold })}>
+    <p className={clsx("font-rubik text-base sm:text-lg xl:text-xl font-light text-wrap text-left antialiased text-gray-800", { "font-normal": bold })}>
       {text}
     </p>
   );
