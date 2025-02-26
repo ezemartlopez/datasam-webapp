78777d41c1e0c9061fdaea70e69db15a23ad0b75
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 22:58:31 -0300

    modificaciones de la base

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,22 @@ import { RootStoreProvider } from "@/components/providers/root-store-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import Navbar from "@/components/shared/navbar";
+
+import { Rubik } from "next/font/google";
+import Footer from "@/components/sections/layout/footer";
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Rubik } from "next/font/google";
-
 export const rubikFont = Rubik({
-  variable: "--font-rubik", // Variable para la fuente en CSS
-  subsets: ["latin"], 
+  variable: "--font-rubik", // Asegúrate de crear una variable para la fuente Rubik
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "DATA SAM",
   description: "DATA SAM una comunidad de estudiantes de licenciatura en ciencia de datos de la UNSAM, donde el conocimiento se comparte y la comunidad se fortalece",
   icons: {
-    icon: "/images/logo_v_claro_final.svg",  // Ruta del ícono en la carpeta 'public'
+    icon: "/svg/DataSamLogo.svg",  // Ruta del ícono en la carpeta 'public'
   },
 };
 
@@ -30,11 +31,12 @@ export default function RootLayout({
       <RootStoreProvider>
         <body className={`${rubikFont.variable} antialiased`}>
           <Navbar />
-          <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 md:gap-8">
+          <main className="flex font-rubik flex-col gap-4 bg-muted/40 md:gap-8">
             {children}
             <Analytics />
             <SpeedInsights />
           </main>
+          <Footer/>
         </body>
       </RootStoreProvider>
     </html>
