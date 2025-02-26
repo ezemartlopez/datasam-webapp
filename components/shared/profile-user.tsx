74b160ddf952fec333edefb0b215dcbdf839684e
ezemartlopez <zenitram.e14@gmail.com>
2025-02-26 16:04:12 -0300

    cambiando tipo de user profile

--- components/shared/profile-user.tsx
+++ components/shared/profile-user.tsx
@@ -4,9 +4,9 @@ import { UserRound } from "lucide-react";
 // Component: profile-user.tsx
 export default function ProfileUser() {
   return (
-    <button type="button" className="h-full flex justify-start items-center gap-2">
-      <UserRound className="size-[22px] text-white"/>
-      <span className="font-rubik text-lg font-light text-white">Iniciar Sesión</span>
-    </button>
+    <div className="h-full flex justify-start items-center gap-2 group">
+      <UserRound className="size-[22px] text-white group-hover:text-blue-500 transition-colors duration-500"/>
+      <span className="font-rubik text-base md:text-lg font-light text-white group-hover:text-blue-500 transition-colors duration-500">Iniciar Sesión</span>
+    </div>
   );
 }
