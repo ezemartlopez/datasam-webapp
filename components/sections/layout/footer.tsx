b1e5759a6525378c108606b152805cd6069ecadc
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 23:01:07 -0300

    nuevo footer de layout

---
+++ components/sections/layout/footer.tsx
@@ -0,0 +1,11 @@
+// Footer.tsx
+function Footer() {
+  return (
+    <footer className="w-full h-auto">
+      <div className="w-full h-[80px] bg-green-600 flex justify-center items-center">
+        <p className="font-rubik text-sm sm:text-base font-light text-center text-white">© 2025 - DATA SAM. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
