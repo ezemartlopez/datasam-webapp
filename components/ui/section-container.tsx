3f96af7511143107fe756cdb8b1ac054d86459d7
ezemartlopez <zenitram.e14@gmail.com>
2025-02-25 23:02:24 -0300

    contendero de secciones de la app

--- components/ui/section-container.tsx
+++ components/ui/section-container.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 // Component: section-container.tsx
-export default function SectionContainer({children, borders = false}: {children: ReactNode, borders?: boolean}) {
+export default function SectionContainer({children, borders = false, startElement = false}: {children: ReactNode, borders?: boolean, startElement?:boolean}) {
   return (
     <section className="w-full flex justify-center">
-      <div className="relative max-w-[1440px] p-[30px] md:p-[80px] xl:p-[150px] w-full h-full flex flex-col gap-14">
-        {borders && (<div className="absolute bottom-0 right-0 size-[30px] md:size-[80px] xl:size-[150px] bg-green-500/50"></div>)}
-        {borders && (<div className="absolute top-0 left-0 size-[30px] md:size-[80px] xl:size-[150px] bg-green-500/50"></div>)}
+      <div className={clsx("relative max-w-[1440px] w-full h-full flex flex-col gap-14", {"p-[30px] md:p-[80px] xl:p-[150px]":!startElement, "px-[30px] md:px-[80px] xl:px-[150px]": startElement})}>
+        {borders && (<div className="absolute bottom-0 right-0 size-[30px] md:size-[80px] xl:size-[150px] bg-green-500"></div>)}
+        {borders && (<div className="absolute top-0 left-0 size-[30px] md:size-[80px] xl:size-[150px] bg-green-500"></div>)}
         {children}
       </div>
     </section>
